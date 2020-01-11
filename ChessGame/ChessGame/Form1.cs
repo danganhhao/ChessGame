f8c7d7424a5175627e4a559516bf6ae22c109e08
anhhaodang <haoda.khtn@gmail.com>
2020-01-11 12:37:42 +0700

    add piece chess

--- ChessGame/ChessGame/Form1.cs
+++ ChessGame/ChessGame/Form1.cs
@@ -12,9 +12,41 @@ namespace ChessGame
 {
     public partial class Form1 : Form
     {
+        private Panel[,] chessBoard;
         public Form1()
         {
             InitializeComponent();
+            InitChessBoard();
+        }
+
+        private void InitChessBoard()
+        {
+            const int distance = 25;
+            const int tileSize = 50;
+            const int gridSize = 8;
+            var clr1 = Color.DarkGray;
+            var clr2 = Color.White;
+            chessBoard = new Panel[gridSize, gridSize];
+            for (var i = 0; i < gridSize; i++)
+            {
+                for (var j = 0; j < gridSize; j++)
+                {
+                    var newPanel = new Panel
+                    {
+                        Size = new Size(tileSize, tileSize),
+                        Location = new Point(tileSize * i + distance, tileSize * j + distance)
+                    };
+                    var bm = new Bitmap(Resource1.BlackKing, new Size(newPanel.Width, newPanel.Height));
+                    newPanel.BackgroundImage = bm;
+                    
+                    Controls.Add(newPanel);
+                    chessBoard[i, j] = newPanel;
+                    if (i % 2 == 0)
+                        newPanel.BackColor = j % 2 != 0 ? clr1 : clr2;
+                    else
+                        newPanel.BackColor = j % 2 != 0 ? clr2 : clr1;
+                }
+            }
         }
     }
 }

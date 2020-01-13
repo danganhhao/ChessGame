ab8bbb2029ab66bef787a543321a36e2691c9a34
1612543 <quitland2010@gmail.com>
2020-01-13 23:30:55 +0700

    Add TypeDefine

---
+++ ChessGame/ChessGame/TypeDefine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGame
+{
+    public enum PieceColor
+    {
+        WHITE,
+        BLACK
+    }
+
+    public enum PieceType
+    {
+        KING,
+        QUEEN,
+        ROOK,
+        BISHOP,
+        KNIGHT,
+        PAWN
+    }
+}

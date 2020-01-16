e3f549286d6986c524bbcd24388e168674f34610
1612543 <quitland2010@gmail.com>
2020-01-16 17:07:06 +0700

    Bổ sung nước đi cho Tượng, Xe, Hậu

--- ChessGame/ChessGame/Data/PiecesClass/Bishop.cs
+++ ChessGame/ChessGame/Data/PiecesClass/Bishop.cs
@@ -16,8 +16,33 @@ namespace ChessGame.Data.PiecesClass
         public override List<Point> GetPossibleMove()
         {
             List<Point> ArrPossibleMove = new List<Point>();
-
+            CheckLoop(ref ArrPossibleMove, 1, 1);
+            CheckLoop(ref ArrPossibleMove, 1, -1);
+            CheckLoop(ref ArrPossibleMove, -1, 1);
+            CheckLoop(ref ArrPossibleMove, -1, -1);
             return ArrPossibleMove;
+        }
+
+        private void CheckLoop(ref List<Point> arrPossibleMove, int v1, int v2)
+        {
+            BoardData board = BoardData.GetInstance();
+            int x = Position.X;
+            int y = Position.Y;
+            while (true)
+            {
+                x += v1;
+                y += v2;
+                if (!board.CheckPositionInBoard(x, y))
+                    return;
+                if (board[x, y] == null)
+                    arrPossibleMove.Add(new Point(x, y));
+                else
+                {
+                    if (board[x, y].Side != Side)
+                        arrPossibleMove.Add(new Point(x, y));
+                    return;
+                }
+            }
         }
     }
 }

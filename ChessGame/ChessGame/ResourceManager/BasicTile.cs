f90b6b1039ab1b6f77be48e81ce323f7d8bc8418
anhhaodang <haoda.khtn@gmail.com>
2020-01-17 14:43:00 +0700

    add tile manager

---
+++ ChessGame/ChessGame/ResourceManager/BasicTile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGame.ResourceManager
+{
+    class BasicTile : CommonTile
+    {
+        public BasicTile() : base()
+        {
+        }
+
+        public override void SetAvalTileResource()
+        {
+            tiles.Add(TILE.AvalBlackTile, Resource1.BlackBishop);
+            tiles.Add(PIECE.WhiteBishop, Resource1.WhiteBishop);
+        }
+
+        public override void SetLastMoveResource()
+        {
+            base.SetLastMoveResource();
+        }
+
+        public override void SetNormalTileResource()
+        {
+            base.SetNormalTileResource();
+        }
+
+        public override void SetSelectTileResource()
+        {
+            base.SetSelectTileResource();
+        }
+    }
+}
---
+++ ChessGame/ChessGame/ResourceManager/CommonTile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGame.ResourceManager
+{
+    enum TILE
+    {
+        NormalBlackTile, NormalWhiteTile,
+        SelectBlackTile, SelectWhiteTile,
+        AvalBlackTile, AvalWhiteTile,
+        LastMoveBlackTile, LastMoveWhiteTile
+    }
+
+    abstract class CommonTile
+    {
+        protected Dictionary<TILE, Bitmap> tiles = new Dictionary<TILE, Bitmap>();
+
+        public CommonTile()
+        {
+            SetNormalTileResource();
+            SetSelectTileResource();
+            SetAvalTileResource();
+            SetLastMoveResource();
+        }
+
+        public Bitmap GetTile(TILE type)
+        {
+            if (tiles.ContainsKey(type))
+            {
+                return tiles[type];
+            }
+            return null;
+        }
+        public virtual void SetNormalTileResource() { }
+        public virtual void SetSelectTileResource() { }
+        public virtual void SetAvalTileResource() { }
+        public virtual void SetLastMoveResource() { }
+    }
+}
